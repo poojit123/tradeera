2e7bc6ef62ae67a1e7191160908f6b69d536586f
jitu patidar <jitu@gmail.com>
2023-06-03 17:18:53 +0530

    Update cpanel yml file

--- include/constant.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(!defined('BASE_URL')){
-        define('BASE_URL','http://tradeera.in');
-    }
-    if(!defined('IMAGES_URL')){
-        define('IMAGES_URL','http://tradeera.in/assets/images');
-    }
-?>
